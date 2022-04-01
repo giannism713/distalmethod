d49a3f75522fa340cb9904d7bf1810218cf1083e
Giannis Markakis <daskalos@pop-os.localdomain>
2022-04-02 01:16:17 +0300

    next

--- assets/js/main.js
+++ assets/js/main.js
@@ -111,6 +111,17 @@
         }
 
     })
+    
 
 
 })(jQuery)
+
+const imgSecond = document.querySelector('.content-right img:last-child');
+
+imgSecond.addEventListener('mouseover', () => {
+    document.querySelector('.content-right img:first-child').style.opacity = 0;
+});
+
+imgSecond.addEventListener('mouseleave', () => {
+    document.querySelector('.content-right img:first-child').style.opacity = 1;
+});
